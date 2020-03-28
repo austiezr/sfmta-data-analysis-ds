93a2be39dca238a964ce28e43b62efa90bd411f2
Jordan Ireland <jordan.b.ireland@gmail.com>
2020-03-27 21:05:59 -0700

    Updated visualization notebooks and uploaded new data for API.

--- jordan-sfmta-api/app.py
+++ jordan-sfmta-api/app.py
@@ -1,17 +1,18 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 import json
 import pandas as pd
-import requests
 import plotly.graph_objects as go
 import plotly.utils as pu
 from decouple import config
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 token = config('MAPBOX_TOKEN')
 file = open('sfmta-api/schedule_data.json')
 schedule_data = pd.read_json(file, orient='split')
-file = open('sfmta-api/stop_data.csv')
-stop_data = pd.read_csv(file)
+file = open('sfmta-api/route_data_new.json')
+new_route_info = pd.read_json(file, orient='split')
 file = open('sfmta-api/route_paths.json')
 path_df = pd.read_json(file, orient='split')
 
@@ -23,45 +24,37 @@ def index():
 
 @app.route("/all-routes", methods=['GET'])
 def all_routes():
-    traces, layout = create_graph()
-    return json.dumps({"traces": traces, 'layout': layout}, cls=pu.PlotlyJSONEncoder)
+    traces, layout, names, types = create_graph()
+    return json.dumps(
+        {"traces": traces, 'layout': layout, 'names': names, 'types': types},
+        cls=pu.PlotlyJSONEncoder)
 
 
-def create_graph():
+@app.route("/type-map", methods=['GET'])
+def type_routes():
+    route_type = request.args.get('type', type=str)
+    traces, layout, names = create_graph(route_type)
+    return json.dumps(
+        {"traces": traces, 'layout': layout, 'names': names},
+                      cls=pu.PlotlyJSONEncoder)
+
+
+@app.route("/testing", methods=['GET'])
+def testing():
+    traces, layout, names, types = create_graph(testing=True)
+    return json.dumps(
+        {"traces": traces, 'layout': layout, 'names': names, 'types': types},
+        cls=pu.PlotlyJSONEncoder)
+
+
+def create_graph(route_type=None, testing=False):
     traces = []
-    routes = [9, 23, 15, 7]
-    symbols = ['rail-metro', 'bus', 'bus', 'rail-light']
-    colors = ['blue', 'red', 'red', 'green']
-    i = 0
-
-    for index, row in path_df.iloc[routes, :].iterrows():
-        stopcoords = {'lats': [], 'lngs': []}
-        for path in row['path']:
-            lats, lngs = [], []
-            for p in path:
-                lats.append(p[0])
-                lngs.append(p[1])
-
-            # Create route lines for individual routes
-            traces.append(go.Scattermapbox(
-                mode="lines",
-                lon=lngs, lat=lats,
-                hoverinfo='none',
-                marker={'color': colors[i]},
-                ids=[row['tag']] * len(path)))
-
-        # Get Bus/tram locations
-        mask = stop_data['route_id'] == row['tag']
-        stopcoords['lats'].extend(stop_data[mask]['lat'])
-        stopcoords['lngs'].extend(stop_data[mask]['lon'])
-        # Create Bus/train stop icons
-        traces.append(go.Scattermapbox(
-            mode="markers",
-            lon=stopcoords['lngs'], lat=stopcoords['lats'],
-            marker={'size': 12, 'color': 'black',
-                    'symbol': [symbols[i]] * len(stopcoords['lats'])},
-            ids=[row['tag']] * len(stopcoords['lats'])))
-        i += 1
+    colors = {'bus': 'blue', 'rapid': 'blue', 'rail': 'red',
+              'streetcar': 'green', 'express': 'blue', 'shuttle': 'yellow',
+              'overnight': 'blue', 'cablecar': 'green'}
+    symbols = {'bus': 'bus', 'rapid': 'bus', 'rail': 'rail-metro',
+               'streetcar': 'rail-light', 'express': 'bus', 'shuttle': 'car',
+               'overnight': 'bus', 'cablecar': 'rail-light'}
 
     layout = go.Layout(
         mapbox_style="outdoors",
@@ -75,7 +68,173 @@ def create_graph():
         height=800
     )
 
-    return traces, layout
+    if route_type is None and testing is False:
+        names = []
+        types = {'bus': []}
+        i, y = 0, 0
+
+        for index, row in path_df.iterrows():
+            stopcoords = {'lats': [], 'lngs': []}
+            mask = new_route_info['route_id'] == row['tag']
+            names.append(
+                {'route_id': row['tag'], 'route_name': row['name'], 'traces': []})
+            for path in row['path']:
+                lats, lngs = [], []
+                for p in path:
+                    lats.append(float(p[0]))
+                    lngs.append(float(p[1]))
+
+                # Create route lines for individual routes
+                traces.append(go.Scattermapbox(
+                    mode="lines",
+                    lon=lngs, lat=lats,
+                    hoverinfo='none',
+                    marker={
+                        'color': colors[new_route_info[mask]['type'].values[0]]}))
+                names[i]['traces'].append(y)
+                y += 1
+
+            # Get Bus/tram locations
+            stopcoords['lats'].extend(new_route_info[mask]['lat'].values[0])
+            stopcoords['lngs'].extend(new_route_info[mask]['lon'].values[0])
+            # Create Bus/train stop icons
+            data = [io.capitalize() for io in new_route_info[mask]["dir"].values[0]]
+
+            traces.append(go.Scattermapbox(
+                mode="markers",
+                lon=stopcoords['lngs'], lat=stopcoords['lats'],
+                marker={'size': 12, 'color': 'black', 'symbol': [symbols[
+                                                                     new_route_info[
+                                                                         mask][
+                                                                         'type'].values[
+                                                                         0]]] * len(
+                    stopcoords['lats'])},
+                text=new_route_info[mask]["title"].values[0],
+                customdata=data,
+                hovertemplate='<b>Name:</b> %{text}<br>' +
+                              '<b>Direction:</b> %{customdata}' +
+                              '<extra></extra>'))
+            names[i]['traces'].append(y)
+            if new_route_info[mask]['type'].values[0] in types.keys():
+                types[new_route_info[mask]['type'].values[0]].append(i)
+            else:
+                types[new_route_info[mask]['type'].values[0]] = [i]
+            y += 1
+            i += 1
+        return traces, layout, names, types
+    elif testing is True:
+        names = []
+        routes = [9,10,21,50,60]
+        types = {'bus': []}
+        i, y = 0, 0
+
+        for index, row in path_df.iloc[routes,:].iterrows():
+            stopcoords = {'lats': [], 'lngs': []}
+            mask = new_route_info['route_id'] == row['tag']
+            names.append(
+                {'route_id': row['tag'], 'route_name': row['name'],
+                 'traces': []})
+            for path in row['path']:
+                lats, lngs = [], []
+                for p in path:
+                    lats.append(float(p[0]))
+                    lngs.append(float(p[1]))
+
+                # Create route lines for individual routes
+                traces.append(go.Scattermapbox(
+                    mode="lines",
+                    lon=lngs, lat=lats,
+                    hoverinfo='none',
+                    marker={
+                        'color': colors[
+                            new_route_info[mask]['type'].values[0]]}))
+                names[i]['traces'].append(y)
+                y += 1
+
+            # Get Bus/tram locations
+            stopcoords['lats'].extend(new_route_info[mask]['lat'].values[0])
+            stopcoords['lngs'].extend(new_route_info[mask]['lon'].values[0])
+            # Create Bus/train stop icons
+            data = [io.capitalize() for io in
+                    new_route_info[mask]["dir"].values[0]]
+
+            traces.append(go.Scattermapbox(
+                mode="markers",
+                lon=stopcoords['lngs'], lat=stopcoords['lats'],
+                marker={'size': 12, 'color': 'black', 'symbol': [symbols[
+                                                                     new_route_info[
+                                                                         mask][
+                                                                         'type'].values[
+                                                                         0]]]
+                                                                * len(
+                    stopcoords['lats'])},
+                text=new_route_info[mask]["title"].values[0],
+                customdata=data,
+                hovertemplate='<b>Name:</b> %{text}<br>' +
+                              '<b>Direction:</b> %{customdata}' +
+                              '<extra></extra>'))
+            names[i]['traces'].append(y)
+            if new_route_info[mask]['type'].values[0] in types.keys():
+                types[new_route_info[mask]['type'].values[0]].append(i)
+            else:
+                types[new_route_info[mask]['type'].values[0]] = [i]
+            y += 1
+            i += 1
+        return traces, layout, names, types
+    else:
+        names = []
+        i, y = 0, 0
+        route_mask = new_route_info['type'] == route_type
+        for index, row in path_df[route_mask].iterrows():
+            stopcoords = {'lats': [], 'lngs': []}
+            mask = new_route_info['route_id'] == row['tag']
+            names.append(
+                {'route_id': row['tag'], 'route_name': row['name'],
+                 'traces': []})
+            for path in row['path']:
+                lats, lngs = [], []
+                for p in path:
+                    lats.append(float(p[0]))
+                    lngs.append(float(p[1]))
+
+                # Create route lines for individual routes
+                traces.append(go.Scattermapbox(
+                    mode="lines",
+                    lon=lngs, lat=lats,
+                    hoverinfo='none',
+                    marker={
+                        'color': colors[
+                            new_route_info[mask]['type'].values[0]]}))
+                names[i]['traces'].append(y)
+                y += 1
+
+            # Get Bus/tram locations
+            stopcoords['lats'].extend(new_route_info[mask]['lat'].values[0])
+            stopcoords['lngs'].extend(new_route_info[mask]['lon'].values[0])
+            # Create Bus/train stop icons
+            data = [io.capitalize() for io in
+                    new_route_info[mask]["dir"].values[0]]
+
+            traces.append(go.Scattermapbox(
+                mode="markers",
+                lon=stopcoords['lngs'], lat=stopcoords['lats'],
+                marker={'size': 12, 'color': 'black', 'symbol': [symbols[
+                                                                     new_route_info[
+                                                                         mask][
+                                                                         'type'].values[
+                                                                         0]]]
+                                                                * len(
+                    stopcoords['lats'])},
+                text=new_route_info[mask]["title"].values[0],
+                customdata=data,
+                hovertemplate='<b>Name:</b> %{text}<br>' +
+                              '<b>Direction:</b> %{customdata}' +
+                              '<extra></extra>'))
+            names[i]['traces'].append(y)
+            y += 1
+            i += 1
+
+        return traces, layout, names
 
 def create_route(coords):
     """Coords: start_lon, start_lat, end_lon, end_lat"""
