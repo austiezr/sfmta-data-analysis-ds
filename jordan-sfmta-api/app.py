ba77db1df808c9afe4f12586871ce8f629ad5ded
austiezr <austinziech@gmail.com>
2020-05-18 12:05:49 -0600

    postgres / mysql swap

--- jordan-sfmta-api/app.py
+++ jordan-sfmta-api/app.py
@@ -1,6 +1,7 @@
 # Flask app providing API links for the web front end to use
 
-from flask import Flask, url_for, request
+from flask import Flask, request
+# from flask import url_for
 import json
 import pandas as pd
 import plotly.graph_objects as go
@@ -8,29 +9,34 @@ import plotly.utils as pu
 from decouple import config as conf
 from flask_cors import CORS
 from datetime import datetime
-import mysql.connector as sql
+# import mysql.connector as sql
+import psycopg2 as pg
+import requests
 
 app = Flask(__name__)
 CORS(app)
+
 token = conf('MAPBOX_TOKEN')
-file = open('sfmta-api/schedule_data.json')
+file = open('schedule_data.json')
 schedule_data = pd.read_json(file, orient='split')
-file = open('sfmta-api/route_data_new.json')
+file = open('route_data_new.json')
 new_route_info = pd.read_json(file, orient='split')
-file = open('sfmta-api/route_paths.json')
+file = open('route_paths.json')
 path_df = pd.read_json(file, orient='split')
 
+# credentials for DB connection
 config = {
   'user': "USER",
   'password': "PASSWORD",
   'host': "HOST",
-  'database': "DATABASE",
-  'raise_on_warnings': True
+  'dbname': "DATABASE",
 }
+
 
 @app.route("/")
 def index():
     return "Hello"
+
 
 # returns a JSON of raw vehicle location data, given a route id
 @app.route('/real-time', methods=['GET'])
@@ -39,10 +45,10 @@ def get_real_time():
     last_call = request.args.get('last', default=None)
 
     vehicles = {'rid': [], 'vid': [], 'lat': [], 'lng': [], 'dir': []}
-    cnx = sql.connect(**config)
+    cnx = pg.connect(**config)
     cursor = cnx.cursor()
 
-    #if last call is none, then return all buses on that route
+    # if last call is none, then return all buses on that route
     if last_call is None:
         query = (
             "SELECT datetime, rid, vid, lat, lon, dir FROM location WHERE "
@@ -65,7 +71,7 @@ def get_real_time():
     else:
         dt = datetime.strptime(str(last_call), '%Y%m%d%H%M%S')
         query = (
-            "SELECT datetime, rid, vid, lat, lon, dir FROM location WHERE rid "
+            "SELECT datetime, rid, vid, lat, lon, dir FROM location WHERE rid"
             "= %s AND `datetime` >= %s GROUP BY vid LIMIT 20")
 
         cursor.execute(query, (str(route_id), str(dt)))
@@ -83,13 +89,20 @@ def get_real_time():
         cursor.close()
         cnx.close()
 
-    return json.dumps({'vehicles': vehicles, 'lastcall': last_call})
+    return json.dumps({'vehicles': vehicles,
+                       'lastcall': last_call})
+
 
 # returns the output of create_graph() in JSON format
 @app.route("/all-routes", methods=['GET'])
 def all_routes():
     traces, layout, names, types = create_graph()
-    return json.dumps({"traces": traces, 'layout': layout, 'names': names, 'types': types}, cls=pu.PlotlyJSONEncoder)
+    return json.dumps({"traces": traces,
+                       'layout': layout,
+                       'names': names,
+                       'types': types},
+                      cls=pu.PlotlyJSONEncoder)
+
 
 # this route returns the "type" argument passed to it
 # possibly unfinished
@@ -98,26 +111,43 @@ def type_routes():
     route_type = request.args.get('type', type=str)
     return route_type
 
+
 # creates the plotly map of San Francisco showing a specific route
 def create_graph():
     traces = []
-    colors = {'bus': 'blue', 'rapid': 'blue', 'rail': 'red',
-              'streetcar': 'green', 'express': 'blue', 'shuttle': 'yellow',
-              'overnight': 'blue', 'cablecar': 'green'}
-    symbols = {'bus': 'bus', 'rapid': 'bus', 'rail': 'rail-metro',
-               'streetcar': 'rail-light', 'express': 'bus', 'shuttle': 'car',
-               'overnight': 'bus', 'cablecar': 'rail-light'}
+    colors = {'bus': 'blue',
+              'rapid': 'blue',
+              'rail': 'red',
+              'streetcar': 'green',
+              'express': 'blue',
+              'shuttle': 'yellow',
+              'overnight': 'blue',
+              'cablecar': 'green'}
+
+    symbols = {'bus': 'bus',
+               'rapid': 'bus',
+               'rail': 'rail-metro',
+               'streetcar': 'rail-light',
+               'express': 'bus',
+               'shuttle': 'car',
+               'overnight': 'bus',
+               'cablecar': 'rail-light'}
+
     names = []
     types = {'bus': []}
     i, y = 0, 0
 
-    for index, row in path_df.iterrows():
+    for col, row in path_df.iterrows():
         stopcoords = {'lats': [], 'lngs': []}
         mask = new_route_info['route_id'] == row['tag']
         names.append(
-            {'route_id': row['tag'], 'route_name': row['name'], 'traces': []})
+            {'route_id': row['tag'],
+             'route_name': row['name'],
+             'traces': []})
+
         for path in row['path']:
             lats, lngs = [], []
+
             for p in path:
                 lats.append(float(p[0]))
                 lngs.append(float(p[1]))
@@ -129,29 +159,31 @@ def create_graph():
                 hoverinfo='none',
                 marker={
                     'color': colors[new_route_info[mask]['type'].values[0]]}))
+
             names[i]['traces'].append(y)
             y += 1
 
         # Get Bus/tram locations
         stopcoords['lats'].extend(new_route_info[mask]['lat'].values[0])
         stopcoords['lngs'].extend(new_route_info[mask]['lon'].values[0])
+
         # Create Bus/train stop icons
-        data = [io.capitalize() for io in new_route_info[mask]["dir"].values[0]]
+        data = [io.capitalize()
+                for io in new_route_info[mask]["dir"].values[0]]
 
         traces.append(go.Scattermapbox(
             mode="markers",
             lon=stopcoords['lngs'], lat=stopcoords['lats'],
-            marker={'size': 12, 'color': 'black', 'symbol': [symbols[
-                                                                 new_route_info[
-                                                                     mask][
-                                                                     'type'].values[
-                                                                     0]]] * len(
-                stopcoords['lats'])},
+            marker={'size': 12,
+                    'color': 'black',
+                    'symbol': [symbols[new_route_info[mask][
+                        'type'].values[0]]] * len(stopcoords['lats'])},
             text=new_route_info[mask]["title"].values[0],
             customdata=data,
             hovertemplate='<b>Name:</b> %{text}<br>' +
                           '<b>Direction:</b> %{customdata}' +
                           '<extra></extra>'))
+
         names[i]['traces'].append(y)
         if new_route_info[mask]['type'].values[0] in types.keys():
             types[new_route_info[mask]['type'].values[0]].append(i)
@@ -174,19 +206,22 @@ def create_graph():
 
     return traces, layout, names, types
 
+
 # gets a list of lat/lon coordinates from the mapbox api
 def create_route(coords):
     """Coords: start_lon, start_lat, end_lon, end_lat"""
     """Get route JSON."""
     base_url = 'https://api.mapbox.com/directions/v5/mapbox/walking/'
     url = base_url + str(coords[0]) + \
-          ',' + str(coords[1]) + \
-          ';' + str(coords[2]) + \
-          ',' + str(coords[3])
+        ',' + str(coords[1]) + \
+        ';' + str(coords[2]) + \
+        ',' + str(coords[3])
+
     params = {
         'geometries': 'geojson',
         'access_token': token
     }
+
     req = requests.get(url, params=params)
     route_json = req.json()['routes'][0]
 
