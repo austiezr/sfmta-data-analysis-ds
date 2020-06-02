ee69b7367c8a711d07533eb97ed3f5f7ad5f0fd1
jonDuke <jonduke90@gmail.com>
2020-06-01 17:53:19 -0700

    Added pattern matching to decide route type based on the id and name

--- AWS_Lambda/route_collector.py
+++ AWS_Lambda/route_collector.py
@@ -7,6 +7,7 @@ import psycopg2 as pg
 from dotenv import load_dotenv
 from datetime import date
 
+
 def load_db():
     """ loads the database and returns the connection object """
 
@@ -21,6 +22,7 @@ def load_db():
 
     # return the database connection
     return pg.connect(**creds)
+
 
 def get_active_routes():
     """ returns a list of active route id's """
@@ -37,6 +39,51 @@ def get_active_routes():
 
     # return the list
     return route_list
+
+
+def get_type(id, name):
+    """ 
+    returns the type of route based on its id and full name
+
+    reverse-engineered from observed patterns since we could not 
+    find a data source for them
+
+    Possible types:
+    Bus, Rail, Streetcar, Express, Cable Car, Shuttle, Overnight, Rapid
+    """
+
+    if id[-1] == 'X':  # if id ends in 'X'
+      return 'Express'
+    
+    if id[-1] == 'R':  # if id ends in 'R'
+      return 'Rapid'
+
+    if name[-3:] == 'Owl' or id[-3:] == 'OWL':  # if id or name ends in 'Owl'
+      return 'Overnight'
+    
+    if 'Bart' in name or 'BART' in name:  # if name includes 'Bart'
+      return 'Shuttle'
+    
+    # Historic streetcar routes, should never be more than these 2
+    # This line should change if they ever get renamed
+    if id in ['E', 'F']:
+      return 'Streetcar'
+    
+    # Purely numeric id's are only busses or shuttles
+    # Additionally, some extra routes were added to temporarily 
+    # replace rail lines, such as id 'LBUS'
+    if id.isnumeric() or id[-3:] == 'BUS' or name[-3:] == 'Bus':
+      return "Bus"
+    
+    # Of the 3 known cable cars, only 1 (C) has 'Cable Car' in the name
+    # The rest have no pattern that can distinguish them from the rail lines
+    # So those 3 are hard-coded
+    if id in ['PH', 'PM', 'C'] or 'Cable Car' in name:
+      return 'Cable Car'
+
+    # else
+    return "Rail"
+
 
 def collect_routes(event, context, verbose=True):
     """
@@ -83,11 +130,12 @@ def collect_routes(event, context, verbose=True):
         if row == None:
             # no route with this rid was found, just insert the new route data
             query = """
-                INSERT INTO routes (rid, route_name, begin_date, content)
-                VALUES (%s, %s, %s, %s);
+                INSERT INTO routes (rid, route_name, route_type, begin_date, content)
+                VALUES (%s, %s, %s, %s, %s);
             """
-            cursor.execute(query, (rid, new_route['route']['title'], 
-                                   date.today().isoformat(), 
+            cursor.execute(query, (rid, new_route['route']['title'],
+                                   get_type(rid, new_route['route']['title']),
+                                   date.today().isoformat(),
                                    json.dumps(new_route)))
 
             if verbose:
@@ -105,11 +153,12 @@ def collect_routes(event, context, verbose=True):
 
             # and insert the new schedule
             query = """
-                INSERT INTO routes (rid, route_name, begin_date, content)
-                VALUES (%s, %s, %s, %s);
+                INSERT INTO routes (rid, route_name, route_type, begin_date, content)
+                VALUES (%s, %s, %s, %s, %s);
             """
-            cursor.execute(query, (rid, new_route['route']['title'], 
-                                   date.today().isoformat(), 
+            cursor.execute(query, (rid, new_route['route']['title'],
+                                   get_type(rid, new_route['route']['title']),
+                                   date.today().isoformat(),
                                    json.dumps(new_route)))
 
             if verbose:
