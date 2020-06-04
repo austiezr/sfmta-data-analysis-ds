1c5098da5b775ec3d0b461f0c63d2c0c557f5893
austiezr <austinziech@gmail.com>
2020-06-04 08:09:30 -0600

    new route to pull daily data for specified date & route

--- sfmta-api/application/app.py
+++ sfmta-api/application/app.py
@@ -171,6 +171,55 @@ def get_daily_usage():
     return json.dumps(elements, sort_keys=False, default=str)
 
 
+@app.route('/daily-route-json', methods=['GET'])
+def get_daily_by_route():
+    """
+     Pulls all data from the specified date as json
+     Expects date as string: YYYY-MM-DD
+     Defaults to previous day if none given
+    """
+    day = request.args.get('day',
+                           default=(date.today() - timedelta(days=1)))
+    day = f'%{day}%'
+
+    route = request.args.get('route',
+                             default='1')
+
+    cnx = pg.connect(**creds)
+    cursor = cnx.cursor()
+
+    query = """
+    SELECT
+    (timestamp AT TIME ZONE 'utc' AT TIME ZONE 'pst'), 
+    rid, vid, age, kph, heading, latitude, longitude, direction
+    FROM locations
+    WHERE (timestamp AT TIME ZONE 'utc' AT TIME ZONE 'pst')::TEXT LIKE %s
+    AND rid = %s
+    ORDER BY timestamp
+    """
+
+    query2 = """
+    SELECT
+    column_name
+    FROM information_schema.columns
+    WHERE table_name = 'locations'
+    """
+
+    cursor.execute(query, (day, route))
+    rows = cursor.fetchall()
+
+    cursor.execute(query2)
+    columns = cursor.fetchall()
+
+    elements = []
+
+    for element in rows:
+        elements.append(
+            {columns[x+1][0]: element[x] for x in range(len(element))})
+
+    return json.dumps(elements, sort_keys=False, default=str)
+
+
 @app.route('/get-route-info', methods=['GET'])
 def get_route_schedule():
     """
